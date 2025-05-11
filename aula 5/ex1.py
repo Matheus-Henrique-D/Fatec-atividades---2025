05e693b5169d0a29a043efeed50715406e1d940f
Matheus Henrique <matheushds2005@gmail.com>
2025-05-11 20:28:11 -0300

    Exercícios Aula 5

---
+++ aula 5/ex1.py
@@ -0,0 +1,11 @@
+def maximo (valor1,valor2):
+    return max  (valor1,valor2)
+
+resposta= maximo(5,6)       #Pode ser interresante fazer em um formato de lista ao inves de 3 variaveis
+resposta1= maximo(2,50)
+resposta2= maximo(7,7)
+
+
+print("resposta",resposta)
+print("resposta",resposta1)
+print("resposta",resposta2)
---
+++ aula 5/ex4.py
@@ -0,0 +1,8 @@
+def triangulo(base,altura):
+    return ((base * altura)/2)
+
+ex= triangulo(6,9)
+ex2= triangulo(5,8)
+
+print(ex)
+print(ex2)
